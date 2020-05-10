7d9eaee37edea5cdf8b110f8310f1493ebe31c88
gio1378 <gio13k@infonie.fr>
2020-05-10 15:42:45 +0200

    init app fonctional

---
+++ web-build/precache-manifest.922573d2ff2babd79311dfcda449c294.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5cd2f32ea4b2b2135b6",
+    "url": "static/js/app.60914c0f.chunk.js"
+  },
+  {
+    "revision": "cd1ec7b9271ad77685d6",
+    "url": "static/js/runtime~app.3005e7fe.js"
+  },
+  {
+    "revision": "c701354bbdd2647f703d",
+    "url": "static/js/2.83d77ecf.chunk.js"
+  },
+  {
+    "revision": "d0c694b562b2208635f250762cd7fc79",
+    "url": "serve.json"
+  },
+  {
+    "revision": "48c7ed4e7da9792af288a60d7242d615",
+    "url": "expo-service-worker.js"
+  },
+  {
+    "revision": "7a7bc7ead25db795e58b336f04d2624c",
+    "url": "favicon.ico"
+  },
+  {
+    "revision": "dc52087d74a6d8cb6b41d95b96a44d42",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "e741ccf1172af3c5c3244dacdffd322c",
+    "url": "apple/icons/icon_192x192.png"
+  },
+  {
+    "revision": "dcaeb12f332621f575c6538b03d13081",
+    "url": "apple/icons/icon_512x512.png"
+  },
+  {
+    "revision": "0167aa1c368409b8b5de6131783645d6",
+    "url": "index.html"
+  },
+  {
+    "revision": "bbbf8ae8051788e9f76e183dcb0f4f72",
+    "url": "static/js/app.60914c0f.chunk.js.gz"
+  },
+  {
+    "revision": "2c8b09622441b0527d664a0aaaf317d5",
+    "url": "static/js/runtime~app.3005e7fe.js.gz"
+  },
+  {
+    "revision": "16709f23227d220b53e235a942bf292c",
+    "url": "expo-service-worker.js.gz"
+  },
+  {
+    "revision": "18158f291da7f3b40d22170aa7375153",
+    "url": "static/js/2.83d77ecf.chunk.js.gz"
+  }
+];

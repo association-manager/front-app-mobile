5a336e64ecb4564c2cfd480937d0aebeafd497de
Nesly <36677244+NeslyPetitBert@users.noreply.github.com>
2020-05-21 19:49:18 +0200

    New application installation

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-/**
- * @format
- */
+import { AppRegistry, Platform } from 'react-native';
+import App from './src/app/app.component';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+AppRegistry.registerComponent('AssociationManager', () => App);
 
-AppRegistry.registerComponent(appName, () => App);
+if (Platform.OS === 'web') {
+  const rootTag = document.getElementById('root') || document.getElementById('main');
+  AppRegistry.runApplication('AssociationManager', { rootTag });
+}
--- metro.config.js
+++ metro.config.js
@@ -1,26 +1,41 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
+const path = require('path');
+const env = require('./env');
 
-const MetroConfig = require('@ui-kitten/metro-config');
+const appModules = [
+  path.resolve(env.EVA_PACKAGES_PATH, 'dss'),
+  path.resolve(env.EVA_PACKAGES_PATH, 'eva'),
+  path.resolve(env.EVA_PACKAGES_PATH, 'material'),
+  path.resolve(env.EVA_PACKAGES_PATH, 'processor'),
+  path.resolve(env.UI_KITTEN_PACKAGES_PATH, 'components'),
+  path.resolve(env.UI_KITTEN_PACKAGES_PATH, 'date-fns'),
+  path.resolve(env.UI_KITTEN_PACKAGES_PATH, 'eva-icons'),
+  path.resolve(env.UI_KITTEN_PACKAGES_PATH, 'moment'),
+];
 
-/**
- * @see https://akveo.github.io/react-native-ui-kitten/docs/guides/improving-performance
- */
-const evaConfig = {
-  evaPackage: '@eva-design/eva',
+const extraNodeModules = {
+  '@babel/runtime': path.resolve(__dirname, './node_modules/@babel/runtime'),
+  'react': path.resolve(__dirname, './node_modules/react'),
+  'react-native': path.resolve(__dirname, './node_modules/react-native'),
+
+  // @ui-kitten/components
+  'fecha': path.resolve(__dirname, './node_modules/fecha'),
+  'hoist-non-react-statics': path.resolve(__dirname, './node_modules/hoist-non-react-statics'),
+  'lodash.merge': path.resolve(__dirname, './node_modules/lodash.merge'),
+  'react-native-svg': path.resolve(__dirname, './node_modules/react-native-svg'),
+
+  // @ui-kitten/date-fns
+  'date-fns': path.resolve(__dirname, './node_modules/date-fns'),
+
+  // @ui-kitten/eva-icons
+  'react-native-eva-icons': path.resolve(__dirname, './node_modules/react-native-eva-icons'),
+
+  // @ui-kitten/moment
+  'moment': path.resolve(__dirname, './node_modules/moment'),
+  'react-is': path.resolve(__dirname, './node_modules/react-is'),
 };
 
-module.exports = MetroConfig.create(evaConfig, {
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
-  },
-});
+module.exports = {
+  projectRoot: path.resolve(__dirname),
+  resolver: { extraNodeModules },
+  watchFolders: appModules,
+};

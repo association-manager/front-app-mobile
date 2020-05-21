78919671ead966428d1a05974ed799a9eeadc81d
Nesly <36677244+NeslyPetitBert@users.noreply.github.com>
2020-05-21 19:58:00 +0200

    Adding new images and creating new icons

--- src/components/extra/icons.tsx
+++ src/components/extra/icons.tsx
@@ -2,85 +2,16 @@ import React from 'react';
 import { ImageStyle } from 'react-native';
 import { Icon, IconElement } from '@ui-kitten/components';
 
-
-export const ProjectTasksListIcon = (style: ImageStyle): IconElement => (
-  <Icon {...style} pack='app' name='tasks-list'/>
-);
-
-export const ProjectTasksListDarkIcon = (style: ImageStyle): IconElement => (
-  <Icon {...style} pack='app' name='tasks-list-dark'/>
-);
-
-export const ProjectsIcon = (style: ImageStyle): IconElement => (
-  <Icon {...style} pack='app' name='projects'/>
-);
-
-export const ProjectsDarkIcon = (style: ImageStyle): IconElement => (
-  <Icon {...style} pack='app' name='projects-dark'/>
-);
-
-export const AssociationIcon = (style: ImageStyle): IconElement => (
-  <Icon {...style} pack='app' name='association'/>
-);
-
-export const AssociationDarkIcon = (style: ImageStyle): IconElement => (
-  <Icon {...style} pack='app' name='association-dark'/>
-);
-
-export const CollaboratorsIcon = (style: ImageStyle): IconElement => (
-  <Icon {...style} pack='app' name='collaborators'/>
-);
-
-export const CollaboratorsDarkIcon = (style: ImageStyle): IconElement => (
-  <Icon {...style} pack='app' name='collaborators-dark'/>
-);
-
-export const HeartIcon = (style: ImageStyle): IconElement => (
-  <Icon {...style} name='heart'/>
-);
-
-export const HomeIcon = (style: ImageStyle): IconElement => (
-  <Icon {...style} name='home'/>
-);
-
-export const CameraIcon = (style: ImageStyle): IconElement => (
-  <Icon {...style} name='camera'/>
-);
-
-export const FileIcon = (style: ImageStyle): IconElement => (
-  <Icon {...style} name='file'/>
-);
-
-export const ImageIcon = (style: ImageStyle): IconElement => (
-  <Icon {...style} name='image'/>
-);
-
-export const MicIcon = (style: ImageStyle): IconElement => (
-  <Icon {...style} name='mic'/>
-);
-
-export const PaperPlaneIcon = (style: ImageStyle): IconElement => (
-  <Icon {...style} name='paper-plane'/>
-);
-
-export const PeopleIcon = (style: ImageStyle): IconElement => (
-  <Icon {...style} name='people'/>
-);
-
-export const PinIcon = (style: ImageStyle): IconElement => (
-  <Icon {...style} name='pin'/>
-);
-
-export const PlusIcon = (style: ImageStyle): IconElement => (
-  <Icon {...style} name='plus'/>
-);
-
 export const EmailIcon = (style: ImageStyle): IconElement => (
   <Icon {...style} name='email'/>
 );
 
 export const LockIcon = (style: ImageStyle): IconElement => (
   <Icon {...style} name='lock'/>
+);
+
+export const TextIcon = (style: ImageStyle): IconElement => (
+  <Icon {...style} name='text'/>
 );
 
 export const GlobeIcon = (style: ImageStyle): IconElement => (
@@ -167,14 +98,6 @@ export const TwitterIcon = (style: ImageStyle): IconElement => (
   <Icon {...style} name='twitter'/>
 );
 
-export const AwardIcon = (style: ImageStyle): IconElement => (
-  <Icon {...style} name='award'/>
-);
-
-export const BulbIcon = (style: ImageStyle): IconElement => (
-  <Icon {...style} name='bulb'/>
-);
-
 export const SearchIcon = (style: ImageStyle): IconElement => (
   <Icon {...style} name='search'/>
 );
@@ -193,6 +116,14 @@ export const StarOutlineIcon = (style: ImageStyle): IconElement => (
 
 export const TrashIcon = (style: ImageStyle): IconElement => (
   <Icon {...style} name='trash'/>
+);
+
+export const AssetAuthIcon = (style: ImageStyle): IconElement => (
+  <Icon {...style} pack='app' name='auth'/>
+);
+
+export const AssetAuthDarkIcon = (style: ImageStyle): IconElement => (
+  <Icon {...style} pack='app' name='auth-dark'/>
 );
 
 export const AssetSocialIcon = (style: ImageStyle): IconElement => (
@@ -217,10 +148,6 @@ export const AssetMessagingIcon = (style: ImageStyle): IconElement => (
 
 export const AssetMessagingDarkIcon = (style: ImageStyle): IconElement => (
   <Icon {...style} pack='app' name='messaging-dark'/>
-);
-
-export const MessageCircleIcon = (style: ImageStyle): IconElement => (
-  <Icon {...style} name='message-circle'/>
 );
 
 export const AssetDashboardsIcon = (style: ImageStyle): IconElement => (

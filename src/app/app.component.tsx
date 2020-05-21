54d15cfa9ffdb8b2e62f7b2e54271b89011d0e7a
Nesly <36677244+NeslyPetitBert@users.noreply.github.com>
2020-05-21 19:52:31 +0200

    New configuration for the new application

--- src/app/app.component.tsx
+++ src/app/app.component.tsx
@@ -1,18 +1,73 @@
 import React from 'react';
+import { AppearanceProvider } from 'react-native-appearance';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
-import { AppNavigator } from './navigation.component';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
+import { AppLoading, LoadFontsTask, Task } from './app-loading.component';
+import { appMappings, appThemes } from './app-theming';
 import { AppIconsPack } from './app-icons-pack';
-import { mapping, dark as darkTheme } from '@eva-design/eva';
-import { default as appTheme } from '../assets/styles/custom-theme.json'; // <-- Import app theme
+import { StatusBar } from '../components/status-bar.component';
+import { SplashImage } from '../components/splash-image.component';
+import { AppNavigator } from '../navigation/app.navigator';
+import { AppStorage } from '../services/app-storage.service';
+import { Mapping, Theme, Theming } from '../services/theme.service';
 
-const theme = { ...darkTheme, ...appTheme };
+const loadingTasks: Task[] = [
+  // Should be used it when running Expo.
+  // In Bare RN Project this is configured by react-native.config.js
+  () => LoadFontsTask({
+    'opensans-regular': require('../assets/fonts/opensans-regular.ttf'),
+    'roboto-regular': require('../assets/fonts/roboto-regular.ttf'),
+  }),
+  () => AppStorage.getMapping(defaultConfig.mapping).then(result => ['mapping', result]),
+  () => AppStorage.getTheme(defaultConfig.theme).then(result => ['theme', result]),
+];
 
-export default () => (
-  <React.Fragment>
-    <IconRegistry icons={[EvaIconsPack, AppIconsPack]}/>
-    <ApplicationProvider mapping={mapping} theme={theme}>
-      <AppNavigator/>
-    </ApplicationProvider>
-  </React.Fragment>
+const defaultConfig: { mapping: Mapping, theme: Theme } = {
+  mapping: 'eva',
+  theme: 'dark',
+};
+
+// const defaultConfig: { mapping: Mapping, theme: Theme } = {
+//   mapping: 'eva',
+//   theme: 'light',
+// };
+const App = ({ mapping, theme }): React.ReactElement => {
+
+  const [mappingContext, currentMapping] = Theming.useMapping(appMappings, mapping);
+  const [themeContext, currentTheme] = Theming.useTheming(appThemes, mapping, theme);
+
+  return (
+    <React.Fragment>
+      <IconRegistry icons={[EvaIconsPack, AppIconsPack]}/>
+      <AppearanceProvider>
+        <ApplicationProvider {...currentMapping} theme={currentTheme}>
+          <Theming.MappingContext.Provider value={mappingContext}>
+            <Theming.ThemeContext.Provider value={themeContext}>
+              <SafeAreaProvider>
+                <StatusBar/>
+                <AppNavigator/>
+              </SafeAreaProvider>
+            </Theming.ThemeContext.Provider>
+          </Theming.MappingContext.Provider>
+        </ApplicationProvider>
+      </AppearanceProvider>
+    </React.Fragment>
+  );
+};
+
+const Splash = ({ loading }): React.ReactElement => (
+  <SplashImage
+    loading={loading}
+    source={require('../assets/images/image-splash.png')}
+  />
+);
+
+export default (): React.ReactElement => (
+  <AppLoading
+    tasks={loadingTasks}
+    initialConfig={defaultConfig}
+    placeholder={Splash}>
+    {props => <App {...props}/>}
+  </AppLoading>
 );

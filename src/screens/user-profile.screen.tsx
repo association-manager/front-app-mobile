8f56446934ffc1be866c0e46c72e7b3d359b6797
Nesly <36677244+NeslyPetitBert@users.noreply.github.com>
2020-05-24 21:27:52 +0200

    user profile management

---
+++ src/screens/user-profile.screen.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ContentView from '../layouts/login-system/user-profile';
+
+export const UserProfileScreen = ({ navigation }): React.ReactElement => (
+  <ContentView navigation={navigation}/>
+);
